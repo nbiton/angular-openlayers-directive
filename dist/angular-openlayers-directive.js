5821229ec74adae688aa023f84372a1c09047435
nbiton <naor.biton@gmail.com>
2015-03-05 12:33:53 +0800

    Release v0.0.4-alpha9

--- dist/angular-openlayers-directive.js
+++ dist/angular-openlayers-directive.js
@@ -638,6 +638,7 @@ angular.module('openlayers-directive')
 
                     if (!scopes.length) {
                         map.removeLayer(mapDict[mapIndex].markerLayer);
+                        delete mapDict[mapIndex];
                     }
                 }
             };
